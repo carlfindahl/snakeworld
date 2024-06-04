9e8029f381c2c45c586f78f47d7afc0110341396
Carl Findahl <sportleroro@gmail.com>
2024-06-04 08:39:48 +0200

    Implement basic Scene Manager

--- include/scenes/scene.h
+++ include/scenes/scene.h
@@ -1,14 +1,36 @@
 #ifndef H_SCENE_H
 #define H_SCENE_H
 
+#define MAX_SCENES 10
+
 /**
  * Scene structure for the game
  */
-typedef struct {
+typedef struct Scene
+{
+    int initialized;
     void (*init)();
     void (*update)();
     void (*draw)();
     void (*uninit)();
 } Scene;
+
+typedef struct SceneManager
+{
+    Scene scene[MAX_SCENES];
+    int top;
+} SceneManager;
+
+SceneManager scene_manager_create();
+
+void scene_manager_push(SceneManager *sm, Scene s);
+
+void scene_manager_pop(SceneManager *sm);
+
+void scene_manager_update(SceneManager *sm);
+
+void scene_manager_draw(SceneManager *sm);
+
+void scene_manager_uninit(SceneManager *sm);
 
 #endif
--- src/scenes/scene.c
+++ src/scenes/scene.c
@@ -1,2 +1,78 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "scenes/scene.h"
 
+SceneManager scene_manager_create()
+{
+    SceneManager sm;
+    sm.top = -1;
+    return sm;
+}
+
+void scene_manager_push(SceneManager *sm, Scene s)
+{
+    if (sm->top == MAX_SCENES - 1)
+    {
+        printf("\nWARN: Scene stack full\n");
+        return;
+    }
+
+    if (!s.initialized)
+    {
+        s.init();
+        s.initialized = 1;
+    }
+
+    sm->top++;
+    sm->scene[sm->top] = s;
+}
+
+void scene_manager_pop(SceneManager *sm)
+{
+    if (sm->top == -1)
+    {
+        return;
+    }
+
+    sm->scene[sm->top].uninit();
+    memset(&sm->scene[sm->top], 0, sizeof(Scene));
+    sm->top--;
+}
+
+void scene_manager_update(SceneManager *sm)
+{
+    if (sm->top == -1)
+    {
+        return;
+    }
+
+    sm->scene[sm->top].update();
+}
+
+void scene_manager_draw(SceneManager *sm)
+{
+    if (sm->top == -1)
+    {
+        return;
+    }
+
+    sm->scene[sm->top].draw();
+}
+
+void scene_manager_uninit(SceneManager *sm)
+{
+    for (int i = sm->top; i > -1; i--)
+    {
+        if (sm->scene[i].initialized)
+        {
+            sm->scene[i].uninit();
+            sm->scene[i].initialized = 0;
+        }
+    }
+
+    memset(sm->scene, 0, sizeof(sm->scene));
+    sm->top = -1;
+}
